4d3b17d0abfa416d3761d4106074b2eca6c86c52
leon gongola <leongongola@gmai.com>
2022-08-09 06:05:45 -0600

    commit

--- script.js
+++ script.js
@@ -66,32 +66,5 @@ function resetAll(){
 
 
 
-// function start() {
-//     stop();
-//     timerInterval = setInterval(function() {
-//       timer += 1/60;
-//       hourVal = Math.floor(timer/3600);
-//       secondVal = Math.floor(timer) - Math.floor(timer/60) * 60;
-//       minuteVal = Math.floor(timer/60);
-//       msVal =  Math.floor((timer - Math.floor(timer))*100);;
-//       seconds.innerHTML = secondVal < 10 ? "0" + secondVal.toString() : secondVal;
-//       minutes.innerHTML = minuteVal < 10 ? "0" + minuteVal.toString() : minuteVal;
-//       hours.innerHTML = hourVal < 10 ? "0" + hourVal.toString() : hourVal;
-//       ms.innerHTML = msVal < 10 ? "0" + msVal.toString() : msVal;
-//     }, 1000/60);
 
-//   }
-
-
-// function stop() {
-//     clearInterval(timerInterval);
-//   }
-
-// function resetAll(){
-//   hours = 0;
-//   minutes = 0;
-//   seconds = 0;
-//   ms = 0;
-//  clearTimeout(timerInterval);
-// }
 
